af6e172bab2bfbbbfddf2c201f632ee581083f7b
Andrey Airson <andreyairson@gmail.com>
2023-07-14 04:00:42 -0300

    feat: add transaction controller

---
+++ server/src/modules/transaction/transaction.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import {
+  CreateTransactionDto,
+  SearchTransactionDto,
+  UpdateTransactionDto,
+} from './dto';
+import { TransactionService } from './transaction.service';
+
+@Controller('transaction')
+export class TransactionController {
+  constructor(private readonly transactionService: TransactionService) {}
+
+  @Post()
+  create(@Body() createTransactionDto: CreateTransactionDto) {
+    return this.transactionService.create(createTransactionDto);
+  }
+
+  @Get()
+  findAll(@Query() query: SearchTransactionDto) {
+    return this.transactionService.findAll({
+      limit: Number(query.limit) || 5,
+      page: Number(query.page) || 1,
+    });
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.transactionService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateTransactionDto: UpdateTransactionDto,
+  ) {
+    return this.transactionService.update(+id, updateTransactionDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.transactionService.remove(+id);
+  }
+}
