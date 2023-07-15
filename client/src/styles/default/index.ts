bc3bf367f3afacb3559abd5e91c6162719d58981
Andrey Airson <andreyairson@gmail.com>
2023-07-15 04:50:11 -0300

    feat: created configurations for styled-components and GlobalStyle

---
+++ client/src/styles/default/index.ts
@@ -0,0 +1,47 @@
+export const defaultTheme = {
+  // Cores primárias
+  primaryColor: '#21C25E',
+  secondaryColor: '#02A35C',
+  // Cores de texto
+  textPrimary: '#333333',
+  textSecondary: '#666666',
+
+  // Cores de destaque
+  success: '#00cc66',
+  sucessDark: '#00994d',
+  sucessLight: '#4dff88',
+  warning: '#ffcc00',
+  warningDark: '#b38f00',
+  error: '#cc0000',
+  errorDark: '#990000',
+  errorLight: '#ff6666',
+
+  // Outras cores
+  gray50: '#FFF',
+  gray100: '#f7f7f7',
+  gray200: '#cccccc',
+  gray300: '#999999',
+  gray400: '#666666',
+  gray500: '#333333',
+
+  green50: '#e5f4ed',
+  green100: '#cceadb',
+  green150: '#b2e0c9',
+  green200: '#99d6b7',
+  green250: '#7fcca6',
+  green300: '#4cb782',
+  green400: '#19a35e',
+  green500: '#00994d',
+  green600: '#003d1e',
+  green700: '#002d17',
+  green800: '#001E0F',
+  green900: '#000F07',
+
+  red400: '#b52f2f',
+
+  accentColor1: '#FFC107', // Amarelo
+  accentColor2: '#E91E63', // Rosa
+  accentColor3: '#2196F3', // Azul
+  accentColor4: '#9C27B0', // Roxo
+  accentColor5: '#FF5722' // Laranja
+}
---
+++ client/src/styles/global/index.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    -webkit-font-smoothing: antialiased;
+
+     input[type=number]::-webkit-inner-spin-button { 
+    -webkit-appearance: none;
+    
+    }
+    input[type=number] { 
+      -moz-appearance: textfield;
+      appearance: textfield;
+
+    }
+    ::-webkit-outer-spin-button,
+    ::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+  }
+  body {
+    font-family: 'Open Sans', sans-serif;
+    font-size: 16px;
+    background: linear-gradient(90deg, #21C25E 0%, #02A35C 100%);
+  -webkit-font-smoothing: antialiased;
+
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    background: none;
+    padding: none;
+  }
+`
