e0ebb215e38efb6e262bba736d0340d218813609
Andrey Airson <andreyairson@gmail.com>
2023-07-15 05:07:36 -0300

    feat: indexing all components

---
+++ client/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Button } from './button'
+export { default as Header } from './header'
+export { default as BackDrop } from './back-drop'
+export { default as ModalTransaction } from './modal-transaction'
