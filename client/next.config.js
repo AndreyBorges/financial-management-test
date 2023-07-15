a249f2776cf801a981f46c5a6bd0f8cd9d7e98ab
Andrey Airson <andreyairson@gmail.com>
2023-07-15 04:44:53 -0300

    chore/fix: fixed style error and added new tools

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    modules: {
+      auto: true,
+      localIdentName: '[local]___[hash:base64:5]'
+    },
+    // Certifique-se de incluir extractCSS: true
+    extractCSS: true
+  }
+}
