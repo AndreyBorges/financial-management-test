7916e995f806a16045061f6f7370c9c1c379d2d9
Andrey Airson <andreyairson@gmail.com>
2023-07-16 08:52:54 -0300

    feat: create skeleton for home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,49 @@
-import { Header } from '@/components'
+import { Button, Header, Modals } from '@/components'
+
+import { useCategories, useTransactions } from '@/hook'
+import { useEffect } from 'react'
 
 export default function Home() {
+  const {
+    state: stateTransactions,
+    handleSetCurrentTransaction,
+    handleDeleteCurrentTransaction,
+    handleGetAllTransactions
+  } = useTransactions()
+  const { handleGetAllCategories, state: stateCategories } = useCategories()
+  const { transactions, refreshData } = stateTransactions
+  const { refreshData: refreshDataCategories } = stateCategories
+
+  useEffect(() => {
+    handleGetAllCategories()
+  }, [refreshDataCategories])
+
+  useEffect(() => {
+    handleGetAllTransactions()
+  }, [refreshData])
+
   return (
     <div>
       <Header />
+      <Modals />
+
+      {transactions.map(transaction => (
+        <div
+          key={transaction.id}
+          style={{
+            display: 'flex',
+            gap: '1rem',
+            marginBlock: '1rem'
+          }}
+        >
+          <span>{transaction.description}</span>
+          <span>{transaction.amount}</span>
+          <span>{transaction.category}</span>
+          <span>{transaction.type}</span>
+          <Button onClick={() => handleSetCurrentTransaction(transaction)}>editar</Button>
+          <Button onClick={() => handleDeleteCurrentTransaction(transaction)}>Deletar</Button>
+        </div>
+      ))}
     </div>
   )
 }
