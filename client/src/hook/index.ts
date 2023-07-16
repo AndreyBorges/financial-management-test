840eb826b174dfa92b3b185a7c97b37aa2374cdb
Andrey Airson <andreyairson@gmail.com>
2023-07-16 08:52:24 -0300

    feat: indexing all hooks

--- client/src/hook/index.ts
+++ client/src/hook/index.ts
@@ -1,3 +1,5 @@
 export { default as useDebounce } from './useDebounce'
 export { default as useTransactions } from './useTransactions'
 export { default as useMediaQuery } from './useMediaQuery'
+export { default as useCategories } from './useCategories'
+export { default as useModal } from './useModal'
