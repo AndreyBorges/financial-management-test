64925b22cbf207eb9da45d4a515405a7d783acab
Andrey Airson <andreyairson@gmail.com>
2023-07-17 05:48:13 -0300

    refactor: improved handleMaskValue function

--- client/src/utils/mask-value.ts
+++ client/src/utils/mask-value.ts
@@ -2,7 +2,6 @@ export const handleMaskValue = (
   value: number,
   setMasked?: (value: React.SetStateAction<string>) => void
 ) => {
-  if (!value) return
   const formattedValue = value.toLocaleString('pt-BR', {
     style: 'currency',
     currency: 'BRL'
