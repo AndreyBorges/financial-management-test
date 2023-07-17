df14d75c583d55d1800709f9336bc0ac7f3d8995
Andrey Airson <andreyairson@gmail.com>
2023-07-17 05:53:08 -0300

    refactor: changed logic and added new rules

--- client/src/components/select/index.tsx
+++ client/src/components/select/index.tsx
@@ -1,32 +1,71 @@
 import { FC, useEffect, useState } from 'react'
-import { ICategory } from '@/interfaces'
+import { ICategory, TransactionType } from '@/interfaces'
 import { StyledSelect } from './styles'
 
 interface ISelectInputProps {
-  options: ICategory[]
+  options: any[]
   onChange: (value: string) => void
-  onBlur: (ev: React.FocusEvent<HTMLInputElement>) => void
+  onBlur?: (ev: React.FocusEvent<HTMLInputElement>) => void
   defaultValue?: {
+    label?: string
+    value?: string
+  }
+  variant?: keyof typeof customStyles
+  placeholder?: string
+  isSearchable?: boolean
+  name?: string
+  backgroundProp?: string
+  colorProp?: string
+  boxShadowProp?: string
+  value?: {
     label?: string
     value?: string
   }
 }
 
 const customStyles = {
-  option: (provided: any, state: any) => ({
-    ...provided,
-    backgroundColor: state.isFocused ? '#001E0F' : '#000f07'
-  }),
-  control: (provided: any, state: any) => ({
-    ...provided,
-    boxShadow: state.isFocused ? '0 0 0 0' : provided.boxShadow,
-    '&:hover': {
-      boxShadow: state.isFocused ? '0 0 0 0' : provided.boxShadow
-    }
-  })
+  primary: {
+    option: (provided: any, state: any) => ({
+      ...provided,
+      backgroundColor: state.isFocused ? '#001E0F' : '#000f07'
+    }),
+    control: (provided: any, state: any) => ({
+      ...provided,
+      boxShadow: state.isFocused ? '0 0 0 0' : provided.boxShadow,
+      '&:hover': {
+        boxShadow: state.isFocused ? '0 0 0 0' : provided.boxShadow
+      }
+    })
+  },
+  secondary: {
+    option: (provided: any, state: any) => ({
+      ...provided,
+      backgroundColor: state.isFocused ? '#99d6b7' : '#fff'
+    }),
+    control: (provided: any, state: any) => ({
+      ...provided,
+      boxShadow: state.isFocused ? '0 0 0 1px #99d6b7' : provided.boxShadow,
+      '&:hover': {
+        boxShadow: state.isFocused ? '0 0 0 1px #99d6b7' : provided.boxShadow
+      }
+    })
+  }
 }
 
-const SelectInput: FC<ISelectInputProps> = ({ options, onChange, onBlur, defaultValue }) => {
+const SelectInput: FC<ISelectInputProps> = ({
+  options,
+  onChange,
+  onBlur,
+  defaultValue,
+  variant = 'primary',
+  placeholder = 'Selecione uma categoria',
+  isSearchable = true,
+  name = 'category',
+  backgroundProp,
+  colorProp,
+  boxShadowProp,
+  value
+}) => {
   const [currOptions, setCurrOptions] = useState<
     {
       label: string
@@ -37,8 +76,8 @@ const SelectInput: FC<ISelectInputProps> = ({ options, onChange, onBlur, default
   useEffect(() => {
     const currOptions = options.map(option => {
       return {
-        label: option.name,
-        value: option.name
+        label: option.label,
+        value: option.value
       }
     })
 
@@ -51,14 +90,18 @@ const SelectInput: FC<ISelectInputProps> = ({ options, onChange, onBlur, default
 
   return (
     <StyledSelect
-      placeholder={'Selecione uma categoria'}
-      isSearchable={true}
+      placeholder={placeholder}
+      isSearchable={isSearchable}
       defaultValue={defaultValue}
-      name='category'
+      name={name}
+      id={name}
+      instanceId={name}
       options={currOptions}
+      value={value}
       onChange={value => handleChange(value as (typeof currOptions)[0])}
       onBlur={onBlur}
-      styles={customStyles}
+      styles={customStyles[variant]}
+      {...{ backgroundProp, colorProp, boxShadowProp }}
     />
   )
 }
--- client/src/components/select/styles.ts
+++ client/src/components/select/styles.ts
@@ -2,20 +2,25 @@ import { css, styled } from 'styled-components'
 import { pxToRem } from '@/utils'
 import Select from 'react-select'
 
-export const StyledSelect = styled(Select)`
-  ${({ theme }) => css`
+interface IStyledSelectProps {
+  backgroundProp?: string
+  colorProp?: string
+  boxShadowProp?: string
+}
+
+export const StyledSelect = styled(Select)<IStyledSelectProps>`
+  ${({ theme, backgroundProp, colorProp, boxShadowProp }) => css`
     > div {
-      color: ${theme.green50};
       width: 100%;
       border-radius: ${pxToRem(6)};
       border: 0;
-      background: ${theme.green900};
-      color: ${theme.green50};
+      background: ${backgroundProp ? backgroundProp : theme.green900};
       padding: 0.688rem 0.5rem;
       font-size: ${pxToRem(16)};
+      box-shadow: ${boxShadowProp ? boxShadowProp : 'none'};
     }
     div {
-      color: ${theme.green50};
+      color: ${colorProp ? colorProp : theme.green50};
     }
   `}
 `
