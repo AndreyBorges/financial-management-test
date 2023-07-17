e229d1926111add5bdc6d42b87a746d69515bc4e
Andrey Airson <andreyairson@gmail.com>
2023-07-17 02:30:59 -0300

    feat: apply TransactionsTable component on home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,17 +1,12 @@
-import { Button, Header, Modals } from '@/components'
+import { Header, Modals, TransactionsTable } from '@/components'
 
 import { useCategories, useTransactions } from '@/hook'
 import { useEffect } from 'react'
 
 export default function Home() {
-  const {
-    state: stateTransactions,
-    handleSetCurrentTransaction,
-    handleDeleteCurrentTransaction,
-    handleGetAllTransactions
-  } = useTransactions()
+  const { state: stateTransactions, handleGetAllTransactions } = useTransactions()
   const { handleGetAllCategories, state: stateCategories } = useCategories()
-  const { transactions, refreshData } = stateTransactions
+  const { refreshData } = stateTransactions
   const { refreshData: refreshDataCategories } = stateCategories
 
   useEffect(() => {
@@ -26,24 +21,7 @@ export default function Home() {
     <div>
       <Header />
       <Modals />
-
-      {transactions.map(transaction => (
-        <div
-          key={transaction.id}
-          style={{
-            display: 'flex',
-            gap: '1rem',
-            marginBlock: '1rem'
-          }}
-        >
-          <span>{transaction.description}</span>
-          <span>{transaction.amount}</span>
-          <span>{transaction.category}</span>
-          <span>{transaction.type}</span>
-          <Button onClick={() => handleSetCurrentTransaction(transaction)}>editar</Button>
-          <Button onClick={() => handleDeleteCurrentTransaction(transaction)}>Deletar</Button>
-        </div>
-      ))}
+      <TransactionsTable />
     </div>
   )
 }
