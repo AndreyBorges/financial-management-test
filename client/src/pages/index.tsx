01dc194db9ca70115aa933bb25b58c25966a1f29
Andrey Airson <andreyairson@gmail.com>
2023-07-18 01:37:49 -0300

    feat: added layout component on home

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,27 +1,12 @@
-import { Header, Modals, TransactionsTable } from '@/components'
+import { Header, Layout, Modals, TransactionsTable } from '@/components'
 
-import { useCategories, useTransactions } from '@/hook'
-import { useEffect } from 'react'
-
-export default function Home() {
-  const { state: stateTransactions, handleGetAllTransactions } = useTransactions()
-  const { handleGetAllCategories, state: stateCategories } = useCategories()
-  const { refreshData } = stateTransactions
-  const { refreshData: refreshDataCategories } = stateCategories
-
-  useEffect(() => {
-    handleGetAllCategories()
-  }, [refreshDataCategories])
-
-  useEffect(() => {
-    handleGetAllTransactions()
-  }, [refreshData])
-
+const Home = () => {
   return (
-    <div>
-      <Header />
-      <Modals />
+    <Layout>
       <TransactionsTable />
-    </div>
+      <Modals />
+    </Layout>
   )
 }
+
+export default Home
