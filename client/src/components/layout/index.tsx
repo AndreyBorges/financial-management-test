94899f5f109077a37238e09b8bc516c366358fc7
Andrey Airson <andreyairson@gmail.com>
2023-07-18 01:29:14 -0300

    feat: created layout generic component

---
+++ client/src/components/layout/index.tsx
@@ -0,0 +1,31 @@
+import { Header, Modals, TransactionsTable, NavBar } from '@/components'
+
+import { useCategories, useTransactions } from '@/hook'
+import { FC, useEffect } from 'react'
+
+const Layout: FC<{
+  children?: React.ReactNode
+}> = ({ children }) => {
+  const { state: stateTransactions, handleGetAllTransactions } = useTransactions()
+  const { handleGetAllCategories, state: stateCategories } = useCategories()
+  const { refreshData } = stateTransactions
+  const { refreshData: refreshDataCategories } = stateCategories
+
+  useEffect(() => {
+    handleGetAllCategories()
+  }, [refreshDataCategories])
+
+  useEffect(() => {
+    handleGetAllTransactions()
+  }, [refreshData])
+
+  return (
+    <>
+      <Header />
+      {children}
+      <Modals />
+    </>
+  )
+}
+
+export default Layout
