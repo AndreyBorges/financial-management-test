fb968ab5645049541e572cff03667327ae636efc
Andrey Airson <andreyairson@gmail.com>
2023-07-18 01:30:13 -0300

    feat: improved backdrop

--- client/src/components/back-drop/styles.ts
+++ client/src/components/back-drop/styles.ts
@@ -1,14 +1,12 @@
-import { css, styled } from 'styled-components'
+import { styled } from 'styled-components'
 
 export const BackDropWrapper = styled.div`
-  ${({ theme }) => css`
-    position: fixed;
-    z-index: 2;
-    background: rgba(0, 15, 7, 0.5);
-    width: 100vw;
-    height: 100vh;
-    left: 0;
-    top: 0;
-    backdrop-filter: blur(3px);
-  `}
+  position: fixed;
+  z-index: 2;
+  background: rgba(0, 15, 7, 0.5);
+  width: 100vw;
+  height: 100vh;
+  left: 0;
+  top: 0;
+  backdrop-filter: blur(3px);
 `
