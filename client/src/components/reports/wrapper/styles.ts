71c0b4ecee9eb0ee2fa903be8502cb692fc29470
Andrey Airson <andreyairson@gmail.com>
2023-07-19 02:22:42 -0300

    feat: ajusted charts position

--- client/src/components/reports/wrapper/styles.ts
+++ client/src/components/reports/wrapper/styles.ts
@@ -8,28 +8,20 @@ export const StyledWrapper = styled.main`
     padding: 0 10px;
     display: flex;
     flex-direction: column;
-    gap: 0.5rem;
-
-    @media (min-width: 750px) {
-      gap: 1rem;
-    }
   `}
 `
 
 export const SecondChartWrapper = styled.div`
+  margin-top: 1.5rem;
   max-width: calc(1220px);
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   justify-content: center;
-  gap: 1rem;
+  gap: 1.5rem;
 
   padding: 0px;
   > div {
     padding: 32px;
-
-    &:last-of-type {
-      grid-column: 3;
-    }
   }
 
   @media (max-width: 1280px) {
@@ -38,34 +30,22 @@ export const SecondChartWrapper = styled.div`
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
-    gap: 1rem;
+    
     > div {
       justify-content: center;
       align-items: center;
-
-      &:last-of-type {
-        grid-column: span 2;
-      }
     }
   }
 
   @media (max-width: 820px) {
     grid-template-columns: 1fr;
     > div {
-      &:last-of-type {
-        grid-column: 1;
-      }
     }
   }
 
   @media (max-width: 425px) {
-    gap: 0.5rem;
-    background: red;
     > div {
       padding: 24px;
     }
-  }
-
-  @media (max-width: 375px) {
   }
 `
