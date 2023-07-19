f8ab2d8d1ff8f26088d760adfb47484663382820
Andrey Airson <andreyairson@gmail.com>
2023-07-19 06:03:32 -0300

    feat: indexing all reports

--- client/src/components/reports/index.ts
+++ client/src/components/reports/index.ts
@@ -1,7 +1,5 @@
 export { default as HeaderReports } from './header'
-export { default as WrapperReports } from './wrapper'
-export { default as WalletResumeChart } from './wallet-resume-chart'
+export { default as MostHighValuesInCategoryChart } from './most-high-values-in-category-chart'
 export { default as TotalTransactionsByCategoryChart } from './total-transactions-by-categoty-chart'
-export { default as MostExpensiveTransactionChart } 
-  from './most-expensive-transaction-chart'
-export { default as MostPopulousCategoryChart } from './most-populous-category-chart'
+export { default as WalletResumeChart } from './wallet-resume-chart'
+export { default as WrapperReports } from './wrapper'
