feb8c728a73bdca9de9ed5fc35b500f58762d2cc
Andrey Airson <andreyairson@gmail.com>
2023-07-19 06:01:35 -0300

    feat: implemented TotalTransactionsByCategoryChart with api

--- client/src/components/reports/total-transactions-by-categoty-chart/index.tsx
+++ client/src/components/reports/total-transactions-by-categoty-chart/index.tsx
@@ -1,26 +1,28 @@
-import React from 'react'
 import { BoxChart } from '@/components'
-import { useTheme } from 'styled-components'
+import { useReports } from '@/hook'
 import { Pie } from 'react-chartjs-2'
 
 const TotalTransactionsByCategoryChart = () => {
-  const theme = useTheme()
+  const { state } = useReports()
+  const labels = state?.totalTransactionsByCategoryChart.label as string[]
+  const data = state?.totalTransactionsByCategoryChart.data as number[]
+  const colors = state?.totalTransactionsByCategoryChart.options.colors as string[]
 
   return (
     <BoxChart title='Total transações por categoria'>
       <Pie
         data={{
-          labels: ['Bonus', 'Salario', 'Online'],
+          labels,
           datasets: [
             {
-              label: 'Entrada',
-              data: [78, 19, 218],
-              backgroundColor: [theme.green450, theme.warning, theme.error],
+              label: 'Total de transações',
+              data: data,
+              backgroundColor: colors,
               borderWidth: 0
             }
           ]
         }}
-        options={{ 
+        options={{
           maintainAspectRatio: false,
           plugins: {
             legend: {
