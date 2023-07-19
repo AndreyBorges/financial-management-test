5ca05b6c349198eaf1d287967fff845b982176df
Andrey Airson <andreyairson@gmail.com>
2023-07-19 06:10:56 -0300

    feat: apply ReportsModule on app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
-import { TransactionModule, CategoryModule } from './modules';
+import { TransactionModule, CategoryModule, ReportsModule } from './modules';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TransactionModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'database.sqlite',
       entities: [`${__dirname}/**/*.entity{.ts,.js}`],
       synchronize: true,
     }),
+    TransactionModule,
     CategoryModule,
+    ReportsModule,
   ],
 })
 export class AppModule {}
